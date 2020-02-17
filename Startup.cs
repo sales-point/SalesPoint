81e6ef833ff4d7d5a9d323092a87624742a4f0e3
pag-2010 <36396082+pag2010@users.noreply.github.com>
2020-02-17 21:26:02 +0300

    #1 Добавил сваггер
    /swagger

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace SalesPoint
 {
@@ -20,9 +21,12 @@ namespace SalesPoint
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sales Point API", Version = "v1" });
+            });
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
@@ -43,6 +47,12 @@ namespace SalesPoint
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales Point V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
