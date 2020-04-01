b63f883ed79527b23a52b1eaee20aca6a785bc30
pag-2010 <36396082+pag2010@users.noreply.github.com>
2020-04-01 17:02:39 +0300

    Исправил ошибку в DataSeeder
    Не инициализировалась бд при первом запуске

---
+++ SalesPoint.Data/Migrations/20200331170738_Orders End.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SalesPoint.Data.Migrations
+{
+    public partial class OrdersEnd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "MenuSets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MenuSetId",
+                table: "MenuItems",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Step",
+                columns: table => new
+                {
+                    StepId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Step", x => x.StepId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Comment = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    StepId = table.Column<int>(nullable: false),
+                    Paid = table.Column<bool>(nullable: false),
+                    CustomerUserId = table.Column<Guid>(nullable: true),
+                    ManagerUserId = table.Column<Guid>(nullable: true),
+                    OrderDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_CustomerUserId",
+                        column: x => x.CustomerUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_ManagerUserId",
+                        column: x => x.ManagerUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Step_StepId",
+                        column: x => x.StepId,
+                        principalTable: "Step",
+                        principalColumn: "StepId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    OrderItemId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderId = table.Column<int>(nullable: false),
+                    MenuItemId = table.Column<int>(nullable: false),
+                    MenuItemCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.OrderItemId);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_MenuItems_MenuItemId",
+                        column: x => x.MenuItemId,
+                        principalTable: "MenuItems",
+                        principalColumn: "MenuItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_MenuSetId",
+                table: "MenuItems",
+                column: "MenuSetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerUserId",
+                table: "Orders",
+                column: "CustomerUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ManagerUserId",
+                table: "Orders",
+                column: "ManagerUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StepId",
+                table: "Orders",
+                column: "StepId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuItems_MenuSets_MenuSetId",
+                table: "MenuItems",
+                column: "MenuSetId",
+                principalTable: "MenuSets",
+                principalColumn: "MenuSetId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuItems_MenuSets_MenuSetId",
+                table: "MenuItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Step");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MenuItems_MenuSetId",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "MenuSetId",
+                table: "MenuItems");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "MenuSets",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
