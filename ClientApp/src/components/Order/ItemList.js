c44f8f1a8df669508203bccc52b609e5b2da86d7
kel16 <buyanelephant@mail.ru>
2020-04-01 13:21:06 +0300

    Страница оформления заказа доделана

--- ClientApp/src/components/Order/ItemList.js
+++ ClientApp/src/components/Order/ItemList.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, ButtonGroup, Button } from '@material-ui/core'
 
 const ItemList = () => {
   const selectedItems = useSelector(state => state.cart.items)
 
-  const sum = selectedItems.length
-    ? selectedItems.reduce(function(previousValue, currentValue) {
-        return {
-          count: previousValue.count + currentValue.count,
-          totalCost:
-            previousValue.count * previousValue.item.price +
-            currentValue.count * currentValue.item.price,
-        }
-      })
-    : 0
+  const sum =
+    selectedItems.length > 0
+      ? selectedItems.reduce(function(previousValue, currentValue) {
+          return {
+            count: previousValue.count + currentValue.count,
+            cost:
+              previousValue.count * previousValue.item.price +
+              currentValue.count * currentValue.item.price,
+          }
+        })
+      : 0
 
   return (
     <>
@@ -34,18 +35,40 @@ const ItemList = () => {
                 <Typography gutterBottom>
                   {index + 1}. {it.item.title}
                 </Typography>
-                {it.count > 1 ? (
-                  <Typography gutterBottom>
-                    Цена: {it.item.price * it.count} P (в количестве {it.count} по {it.item.price} P
-                    за штуку)
-                  </Typography>
-                ) : (
-                  <Typography gutterBottom>Цена: {it.item.price} P</Typography>
-                )}
+                <Grid item container direction="row" spacing={2}>
+                  <Grid item>
+                    {it.count > 1 ? (
+                      <Typography gutterBottom>
+                        Цена: {it.item.price * it.count} P (в количестве {it.count} по{' '}
+                        {it.item.price} P за штуку)
+                      </Typography>
+                    ) : (
+                      <Typography gutterBottom>Цена: {it.item.price} P</Typography>
+                    )}
+                  </Grid>
+                  <Grid item>
+                    <ButtonGroup color="secondary">
+                      <Button size="small" variant="contained">
+                        +
+                      </Button>
+                      {it.count > 1 ? (
+                        <Button size="small" variant="contained">
+                          -
+                        </Button>
+                      ) : (
+                        <></>
+                      )}
+                      <Button size="small" variant="contained">
+                        Убрать товар
+                      </Button>
+                    </ButtonGroup>
+                  </Grid>
+                </Grid>
               </Grid>
             ))}
             <Typography gutterBottom>
-              <b>Итого к оплате:</b> {sum.totalCost} рублей
+              <b>Итого к оплате:</b>{' '}
+              {sum.cost ? sum.cost : selectedItems[0].item.price * selectedItems[0].count} рублей
             </Typography>
           </>
         )}
